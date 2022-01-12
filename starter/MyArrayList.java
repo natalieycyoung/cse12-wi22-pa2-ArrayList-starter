eb809f1e251f000d4d3f503fe9df2ca08429cc44
sanj06 <52536720+sanj06@users.noreply.github.com>
2022-01-11 21:32:11 -0800

    Create MyArrayList.java

---
+++ starter/MyArrayList.java
@@ -0,0 +1,17 @@
+/**
+ * TODO: Add your file header
+ * Name:
+ * ID:
+ * Email:
+ * Sources used: Put "None" if you did not have any external help
+ * Some example of sources used would be Tutors, Zybooks, and Lecture Slides
+ * 
+ * 2-4 sentence file description here
+ */
+
+ /**
+ * TODO: Add your implementation of MyArrayList here
+ */
+
+ // Hint: the 'capacity' (length of data array) is not the same as the 'size'
+ // Size is the number of elements in the ArrayList, i.e., the number of valid elements in the array
