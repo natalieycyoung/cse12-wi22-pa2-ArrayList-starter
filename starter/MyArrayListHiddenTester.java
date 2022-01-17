d848c86341fe20ed3e8b8ebfba7ada8f3a140de2
natalieycyoung <44858512+natalieycyoung@users.noreply.github.com>
2022-01-17 14:08:24 -0800

    Implemented setUp(), Replaced Spaces with Tabs

--- starter/MyArrayListHiddenTester.java
+++ starter/MyArrayListHiddenTester.java
@@ -17,111 +17,115 @@ import org.junit.*;
 
 public class MyArrayListHiddenTester {
 
-    /**
-     * This sets up the test fixture. JUnit invokes this method before
-     * every testXXX method. The @Before tag tells JUnit to run this method
-     * before each test */
-    @Before
-    public void setUp() throws Exception {
-        
-    }
+	/**
+	 * This sets up the test fixture. JUnit invokes this method before
+	 * every testXXX method. The @Before tag tells JUnit to run this method
+	 * before each test */
+	@Before
+	public void setUp() throws Exception {
+		listEmpty = new MyArrayList();
+		listDefaultCap = new MyArrayList(DEFAULT_CAPACITY);
+		listCustomCapacity = new MyArrayList(MY_CAPACITY);
+		listWithNull = new MyArrayList(arr);
+		listWithInt = new MyArrayList<Integer>(arrInts);
+	}
 
-    /**
-     * Aims to test the capacity argument constructor when the input
-     * is not valid
-     */
-    @Test
-    public void testConstructorInvalidArg(){
-        
-    }
+	/**
+	 * Aims to test the capacity argument constructor when the input
+	 * is not valid
+	 */
+	@Test
+	public void testConstructorInvalidArg(){
+		
+	}
 
-    /**
-     * Aims to test the Array argument constructor when the input
-     * is null
-     */
-    @Test
-    public void testConstructorNullArg(){
-        
-    }
+	/**
+	 * Aims to test the Array argument constructor when the input
+	 * is null
+	 */
+	@Test
+	public void testConstructorNullArg(){
+		
+	}
 
-    /**
-     * Aims to test the append method when an element is added to a full list
-     * Check reflection on size and capacity
-     */
-    @Test
-    public void testAppendAtCapacity(){
-        
-    }
+	/**
+	 * Aims to test the append method when an element is added to a full list
+	 * Check reflection on size and capacity
+	 */
+	@Test
+	public void testAppendAtCapacity(){
+		
+	}
 
-    /**
-     * Aims to test the prepend method when a null element is added
-     * Checks reflection on size and capacity
-     * Checks whether null was added successfully
-     */
-    @Test
-    public void testPrependNull(){
-        
-    }
-    
-    /**
-     * Aims to test the insert method when input index is out of bounds
-     */
-    @Test
-    public void testInsertOutOfBound(){
-       
-    }
+	/**
+	 * Aims to test the prepend method when a null element is added
+	 * Checks reflection on size and capacity
+	 * Checks whether null was added successfully
+	 */
+	@Test
+	public void testPrependNull(){
+		
+	}
+	
+	/**
+	 * Aims to test the insert method when input index is out of bounds
+	 */
+	@Test
+	public void testInsertOutOfBound(){
+	   
+	}
 
-    /**
-     * Insert multiple (eg. 1000) elements sequentially beyond capacity -
-     * Check reflection on size and capacity
-     * Hint: for loop could come in handy
-     */
-    @Test
-    public void testInsertMultiple(){
-        
-    }
+	/**
+	 * Insert multiple (eg. 1000) elements sequentially beyond capacity -
+	 * Check reflection on size and capacity
+	 * Hint: for loop could come in handy
+	 */
+	@Test
+	public void testInsertMultiple(){
+		
+	}
 
-    /**
-     * Aims to test the get method when input index is out of bound
-     */
-    @Test
-    public void testGetOutOfBound(){
-        
-    }
+	/**
+	 * Aims to test the get method when input index is out of bound
+	 */
+	@Test
+	public void testGetOutOfBound(){
+		
+	}
 
-    /**
-     * Aims to test the set method when input index is out of bound
-     */
-    @Test
-    public void testSetOutOfBound(){
-        
-    }
+	/**
+	 * Aims to test the set method when input index is out of bound
+	 */
+	@Test
+	public void testSetOutOfBound(){
+		
+	}
 
 
-    /**
-     * Aims to test the remove method when input index is out of bound
-     */
-    @Test
-    public void testRemoveOutOfBound(){
-        
-    }
+	/**
+	 * Aims to test the remove method when input index is out of bound
+	 */
+	@Test
+	public void testRemoveOutOfBound(){
+		
+	}
 
-    /**
-     * Aims to test the expandCapacity method when 
-     * requiredCapacity is strictly less than the current capacity
-     */
-    @Test
-    public void testExpandCapacitySmaller(){
-       
-    }
+	/**
+	 * Aims to test the expandCapacity method when 
+	 * requiredCapacity is strictly less than the current capacity
+	 */
+	@Test
+	public void testExpandCapacitySmaller(){
+	   
+	}
 
-    /**
-     * Aims to test the expandCapacity method when 
-     * requiredCapacity is greater than double(2x) the current capacity
-     */
-    @Test
-    public void testExpandCapacityExplode(){
-        
-    }
+	/**
+	 * Aims to test the expandCapacity method when 
+	 * requiredCapacity is greater than double(2x) the current capacity
+	 */
+	@Test
+	public void testExpandCapacityExplode(){
+		
+	}
 
 }
